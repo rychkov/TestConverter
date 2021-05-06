c60222495fd1f4f9f93c02d195f157bc447820f6
Yuri Rychkov <yuri.rychkov@gmail.com>
2021-05-06 14:05:21 +0300

    Over engineering Spring injection. Fix groupId for plugins in pom.

--- src/main/java/uedp/ConverterApp.java
+++ src/main/java/uedp/ConverterApp.java
@@ -1,20 +1,36 @@
 package uedp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import uedp.logic.CsvProcessor;
 import uedp.logic.DataProcessor;
 import uedp.logic.JsonProcessor;
 import uedp.logic.writer.InfoConverter;
-import uedp.logic.writer.JsonConverter;
 
 import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class ConverterApp {
+@SpringBootApplication
+public class ConverterApp implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory.getLogger(ConverterApp.class);
     private static ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
+    @Autowired
+    private InfoConverter infoConverter;
+
     public static void main(String[] args) {
-        InfoConverter infoConverter = new JsonConverter();
+        SpringApplication.run(ConverterApp.class, args);
+        LOG.info("APPLICATION FINISHED");
+    }
+
+    @Override
+    public void run(String... args) {
         for (String name : args) {
             File f = new File(name);
             if (f.exists() && f.isFile()) {
@@ -28,8 +44,12 @@ public class ConverterApp {
                 if (processor != null) {
                     executorService.submit(processor);
                 } else {
-                    // TODO log no processor
+                    LOG.warn("No data processor for file {}", f);
                 }
+            } else if (!f.exists()) {
+                LOG.warn("File [{}] not exist", f);
+            } else if (f.isDirectory()) {
+                LOG.warn("[{}] is directory", f);
             }
         }
         executorService.shutdown();
--- src/main/java/uedp/logic/writer/JsonConverter.java
+++ src/main/java/uedp/logic/writer/JsonConverter.java
@@ -2,8 +2,10 @@ package uedp.logic.writer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
 import uedp.dto.InfoDto;
 
+@Component
 public class JsonConverter implements InfoConverter {
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
